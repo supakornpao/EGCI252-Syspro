2f9c17873a9e3de3a52246a0a6dda8d0d153a537
supakornpao <146055511+supakornpao@users.noreply.github.com>
2025-03-07 08:30:50 +0000

    shared memory

--- shared_memory/consumer.c
+++ shared_memory/consumer.c
@@ -17,8 +17,11 @@ int main(){
   int running = 1;
   void *sh_mem = NULL;
   struct shm_st *sh_area;
+  key_t key;
+  
   srand((unsigned int) getpid());
-  int shmid = shmget((key_t)1234, MEM_SIZE, 0666|IPC_CREAT);
+  key = ftok("/home/runner/", 1234);
+  int shmid = shmget(key, MEM_SIZE, 0666|IPC_CREAT);
 
   if(shmid == -1){
     fprintf(stderr, "shmget failed\n");
@@ -36,11 +39,12 @@ int main(){
   while(running){
     if(sh_area->written){
       printf("Data is written in shared memory: %s\n",sh_area -> data);
+      sh_area->written = 0;
       if(strncmp(sh_area->data, "end",3)==0)
         running = 0;
     
     }
-    sleep(rand()%4);
+    sleep(1);
   }
   if(shmdt(sh_mem) == -1|| shmctl(shmid,IPC_RMID,0)==-1){
     fprintf(stderr, "shmdt or shmctl failed\n");
