58373d97546c1724f5256e081491aaffd03e5af7
supakornpao <146055511+supakornpao@users.noreply.github.com>
2025-03-15 15:57:33 +0000

    test

--- mapped_memory/mchat.c
+++ mapped_memory/mchat.c
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <string.h>
+#include <wait.h>
 
 #define FILE_LENGTH 0x100
 #define FILE_NAME "/tmp/chat-log"
@@ -17,8 +18,20 @@ int main(int argc, char *argv[]){
 
   char buffer[BUFSIZ];
 
-  fd = open(FILE_NAME, O_RDWR, S_IRUSR | S_IWUSR);
+  fd = open(FILE_NAME, O_RDWR|O_CREAT, S_IRUSR | S_IWUSR);
+  if (fd == -1) {
+      perror("File open failed");
+      exit(EXIT_FAILURE);
+  }
+  if (ftruncate(fd, FILE_LENGTH) == -1) {
+      perror("ftruncate failed");
+      exit(EXIT_FAILURE);
+  }
   file_memory = mmap(0, FILE_LENGTH, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (file_memory == MAP_FAILED) {
+      perror("mmap failed");
+      exit(EXIT_FAILURE);
+  }
   close(fd);
 
   if(user == 1){
@@ -29,17 +42,20 @@ int main(int argc, char *argv[]){
         perror("Forking failed");
         exit(EXIT_FAILURE);
       case 0: while(strncmp(buffer, "end chat", 8)){
-                sscanf(file_memory, "%s", buffer);
+                if (strlen((char *)file_memory) > 0) {
+                  sscanf(file_memory, "%255s", buffer);
+                }
                 printf("%s\n", buffer);
+                sleep(1);
               }
               munmap(file_memory, FILE_LENGTH);
               break;
       default:while(strncmp(buffer, "end chat", 8)){
-                fgets(buffer, sizeof(buffer), stdin);
+                fgets(buffer, BUFSIZ, stdin);
                 lseek(fd, FILE_LENGTH + 1, SEEK_SET); 
                 write(fd, "", 1);
-                lseek(fd, 0, SEEK_SET);
-                sprintf((char *)file_memory, "%s", buffer);
+                //lseek(fd, 0, SEEK_SET);
+                snprintf((char *)file_memory, FILE_LENGTH, "%s", buffer);
               }
               munmap(file_memory, FILE_LENGTH);
               break;
@@ -54,17 +70,20 @@ int main(int argc, char *argv[]){
         perror("Forking failed");
         exit(EXIT_FAILURE);
       case 0: while(strncmp(buffer, "end chat", 8)){
-                sscanf(file_memory, "%s", buffer);
+                if (strlen((char *)file_memory) > 0) {
+                  sscanf((char *)file_memory, "%255s", buffer);
+                }
                 printf("%s\n", buffer);
+                sleep(1);
               }
               munmap(file_memory, FILE_LENGTH);
               break;
       default:while(strncmp(buffer, "end chat", 8)){
-                fgets(buffer, sizeof(buffer), stdin);
+                fgets(buffer, BUFSIZ, stdin);
                 lseek(fd, FILE_LENGTH + 1, SEEK_SET); 
                 write(fd, "", 1);
-                lseek(fd, 0, SEEK_SET);
-                sprintf((char *)file_memory, "%s", buffer);
+                //lseek(fd, 0, SEEK_SET);
+                snprintf((char *)file_memory, FILE_LENGTH, "%s", buffer);
               }
               munmap(file_memory, FILE_LENGTH);
               break;
