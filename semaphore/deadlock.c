2bd01e4ba5c338c2a6326b49f64ab6656d652a22
supakornpao <146055511+supakornpao@users.noreply.github.com>
2025-03-21 11:12:57 +0000

    added 2 semaphore

--- semaphore/deadlock.c
+++ semaphore/deadlock.c
@@ -5,12 +5,12 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-static int set_semvalue(void);
-static void del_semvalue(void);
-static int semaphore_p(void);
-static int semaphore_v(void);
+static int set_semvalue(int);
+static void del_semvalue(int);
+static int semaphore_p(int);
+static int semaphore_v(int);
 
-static int sem_id;
+static int s1,s2;
 
 union semun{
   int val;
@@ -22,19 +22,20 @@ union semun{
 void f2(pid_t);
 
 void f1(pid_t pid){
-  semaphore_p();
+  semaphore_p(s1);
   if(pid) f2(pid);
-  semaphore_v();
+  semaphore_v(s1);
 }
 
 void f2(pid_t pid){
-  semaphore_p();
+  semaphore_p(s2);
   if(!pid) f1(pid);
-  semaphore_v();
+  semaphore_v(s2);
 }
 
 int main(int argc, char *argv[]){
-  sem_id = semget((key_t)1234, 1, 0666 | IPC_CREAT);
+  s1 = semget((key_t)1234, 1, 0666 | IPC_CREAT);
+  s2 = semget((key_t)5678, 1, 0666 | IPC_CREAT);
   pid_t pid;
   pid = fork();
 
@@ -48,41 +49,41 @@ int main(int argc, char *argv[]){
   
 }
 
-static int set_semvalue(void) {
+static int set_semvalue(int s) {
   union semun sem_union;
   sem_union.val = 1;
-  if (semctl(sem_id, 0, SETVAL, sem_union) == -1)
+  if (semctl(s, 0, SETVAL, sem_union) == -1)
     return (0);
   return (1);
 }
 
-static void del_semvalue(void) {
+static void del_semvalue(int s) {
   union semun sem_union;
 
-  if (semctl(sem_id, 0, IPC_RMID, sem_union) == -1)
+  if (semctl(s, 0, IPC_RMID, sem_union) == -1)
     fprintf(stderr, "Failed to delete semaphore\n");
 }
 
-static int semaphore_p(void) {
+static int semaphore_p(int s) {
   struct sembuf sem_b;
 
   sem_b.sem_num = 0;
   sem_b.sem_op = -1; /* P() */
   sem_b.sem_flg = SEM_UNDO;
-  if (semop(sem_id, &sem_b, 1) == -1) {
+  if (semop(s, &sem_b, 1) == -1) {
     fprintf(stderr, "semaphore_p failed\n");
     return (0);
   }
   return (1);
 }
 
-static int semaphore_v(void) {
+static int semaphore_v(int s) {
   struct sembuf sem_b;
 
   sem_b.sem_num = 0;
   sem_b.sem_op = 1; /* V() */
   sem_b.sem_flg = SEM_UNDO;
-  if (semop(sem_id, &sem_b, 1) == -1) {
+  if (semop(s, &sem_b, 1) == -1) {
     fprintf(stderr, "semaphore_v failed\n");
     return (0);
   }
